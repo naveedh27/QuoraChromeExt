3c00ad623ddbf33feb5ada91f0302b8aec7c0179
naveedh27 <naveedh27@gmail.com>
2016-01-23 16:49:29 +0530

    First Commit

---
+++ content.js
@@ -0,0 +1,20 @@
+
+console.log("Quora Opened.");
+
+jQuery('.LoggedInSiteHeader').hide();
+jQuery('.layout_3col_left').hide();
+jQuery('.layout_3col_right').hide();
+jQuery('html').css("font-family","calibri");
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+	   //chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+    }
+  }
+);
+
